486d0ed5b3de937e63c10f0418aeef8523064fbb
Erwin Bofetiado <erwinjbofetiado@gmail.com>
2023-09-13 18:09:14 -0500

    max_profit

---
+++ max_profix.js
@@ -0,0 +1,17 @@
+const maxProfit = (prices) => {
+	var max = 0;
+	
+	let left = 0;
+	let right;
+	for (let i = 0; i < prices.length; i++) {
+		if (prices[left] < prices[i]){
+				
+				
+		}
+
+
+	}
+
+	return max;
+
+}
--- valid_palindrome 2.js
+++ valid_palindrome 2.js
@@ -1,9 +1,9 @@
 const isPalindrome = (s) => {
 
   const letters = s.split("").filter(char =>
-    (char.charCodeAt(0) >= 65 && char.charCodeAt(0) <= 90) || (char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 122) ||  char.charCodeAt(0) >= 48 && char.charCodeAt(0) <= 57).join("");
+    (char.charCodeAt(0) >= 65 && char.charCodeAt(0) <= 90) || (char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 122) || char.charCodeAt(0) >= 48 && char.charCodeAt(0) <= 57).join("");
 
-if (letters.length == 1) return true;
+  if (letters.length == 1) return true;
 
   let l = 0;
   let r = letters.length - 1;
@@ -19,3 +19,4 @@ if (letters.length == 1) return true;
   }
   return true;
 }
+
