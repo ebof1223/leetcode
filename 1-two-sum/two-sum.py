95978cca9b3ecd41d8bf402b2245f239a4f7a97d
ebof1223 <66833914+ebof1223@users.noreply.github.com>
2023-11-19 12:35:52 -0600

    Time: 62 ms (76.03%) | Memory: 18 MB (5.97%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,20 +1,10 @@
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
-        # Create a hash map to store indices of elements
-        index_map = {}
         
-        # Iterate through the array
-        for i, num in enumerate(nums):
-            # Calculate the difference between target and current element
-            diff = target - num
-            
-            # Check if the difference is in the hash map
-            if diff in index_map:
-                # If found, return the indices
-                return [index_map[diff], i]
-            
-            # Otherwise, add the current element and its index to the hash map
-            index_map[num] = i
-        
-        # Return an empty list if no solution is found
-        return 
+        bank = dict()
+        for i, el in enumerate(nums):
+            if el in bank: return [bank[el],i]
+            else:
+                pair = target - el
+                bank[pair] = i
+         
