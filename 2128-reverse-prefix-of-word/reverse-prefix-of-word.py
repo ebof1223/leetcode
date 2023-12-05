77bf764956b544ff806e3e7f932691bbb3d5b548
ebof1223 <66833914+ebof1223@users.noreply.github.com>
2023-12-05 13:49:31 -0600

    Time: 33 ms (85.92%) | Memory: 16.3 MB (16.12%) - LeetSync

--- 2128-reverse-prefix-of-word/reverse-prefix-of-word.py
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.py
@@ -2,4 +2,16 @@ class Solution:
     def reversePrefix(self, word: str, ch: str) -> str:
         if ch not in word:
             return word
-        return word[:word.index(ch) + 1][::-1] + word[word.index(ch)+ 1:]
+
+        split = list(word)
+
+        left, right = 0, word.index(ch)
+
+        while left <= right:
+            tmp = word[left]
+            split[left] = split[right]
+            split[right] = tmp
+            left += 1
+            right -= 1
+            
+        return ''.join(split)
