ceb238852d3d3a6732d4f52b7ff4f2344ef36bf1
ebof1223 <66833914+ebof1223@users.noreply.github.com>
2023-12-05 13:43:44 -0600

    Time: 31 ms (91.16%) | Memory: 16.4 MB (16.12%) - LeetSync

---
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.py
@@ -0,0 +1,5 @@
+class Solution:
+    def reversePrefix(self, word: str, ch: str) -> str:
+        if ch not in word:
+            return word
+        return word[:word.index(ch) + 1][::-1] + word[word.index(ch)+ 1:]
