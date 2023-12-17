42afc97d989a592dc5c7ba40f7f0bf66abe42117
ebof1223 <66833914+ebof1223@users.noreply.github.com>
2023-12-17 13:15:14 -0600

    Time: 274 ms (16.61%) | Memory: 20.6 MB (16.44%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,12 +1,12 @@
 from collections import defaultdict
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        counts = defaultdict(int)
-        counts[0] = 1
-        ans = curr = 0
-
+        hash_map = defaultdict(int)
+        hash_map[0] = 1
+        curr = ans = 0
         for num in nums:
             curr += num
-            ans += counts[curr - k]
-            counts[curr] += 1
+            ans += hash_map[curr - k]
+            hash_map[curr] += 1
         return ans
+
